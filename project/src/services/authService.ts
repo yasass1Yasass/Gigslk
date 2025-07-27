1153fdc12766ac56f305a8b1dd1dfaf8cd78e7e4
yasass1Yasass <yasassriweerasen@gmail.com>
2025-07-27 13:40:28 +0530

    changes

--- project/src/services/authService.ts
+++ project/src/services/authService.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api/auth'; 
+const API_URL = 'https://gigslk-backend-production.up.railway.app';
 
 // Function to register a new user
 const register = async (email: string, password: string, username: string, role: string) => {
